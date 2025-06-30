e70eedc51fa1df58c866e7aa5213b296e2f6b610
Kim SAVAROCHE <ksavaroche@innovationk.fr>
2025-06-30 17:48:39 +0200

    fresh strat nodes

--- ethereum/launch.sh
+++ ethereum/launch.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Ethereum"
+
+MAX_NODES=3
+
+for (( iNode=0; iNode < $MAX_NODES; ++iNode ))
+do
+    echo "Deleting node $iNode..."
+    rm -rf "node$iNode"
+    
+    echo "Creating node $iNode..."
+    mkdir "node$iNode"
+done
