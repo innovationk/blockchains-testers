bec26210c9bee8c3816c2b581391b6b7c2244b03
Kim SAVAROCHE <ksavaroche@innovationk.fr>
2025-07-01 15:29:17 +0200

    generate keys

--- ethereum/launch.sh
+++ ethereum/launch.sh
@@ -8,16 +8,28 @@ for (( iNode=0; iNode < $MAX_NODES; ++iNode ))
 do
     echo "Deleting node $iNode..."
     rm -rf "node$iNode"
-    
+done
+
+generatedKeys=""
+
+for (( iNode=0; iNode < $MAX_NODES; ++iNode ))
+do
     echo "Creating node $iNode..."
-    mkdir -p "node$iNode/data"
+    mkdir -p "node$iNode"
 
     echo "Creating account node $iNode..."
-    # Create a password file
-    echo "password$iNode" > "node$iNode/password.txt"
-    geth --datadir "node$iNode/data" account new --password "node$iNode/password.txt"
-    geth --datadir "node$iNode/data" account list
+    password="pwdwallet$iNode"
+
+    geth --datadir "node$iNode" account new --password <(echo -n $password) >> node$iNode/account.logs
+    echo -n "$password" > node$iNode/password.txt
+
+    publicKey=`cat node$iNode/account.logs | grep -i "Public address of the key:   " | cut -d":" -f2 | tr -d '[:space:]'` 
+    echo -n $publicKey > node$iNode/publicKey.txt
+    generatedKeys="$generatedKeys$publicKey\n"
 
     # echo "Initialising node $iNode..."
-    # geth --datadir node$iNode/data init genesis.json
+    # geth --datadir "node$iNode" init genesis.json
 done
+
+echo "Generated publick keys:"
+printf $generatedKeys
